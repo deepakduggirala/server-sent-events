d7812596487d06cb5228c471bd5327afc31ae263
Deepak Duggirala <deepak.duggirala@target.com>
2017-08-31 22:09:30 +0530

    add server sent events routes, controllers, client controllers

--- app.js
+++ app.js
@@ -6,10 +6,32 @@ const port = 8001;
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
 
-app.get('/test', (req, res)=>{
-    res.send('Hello');
+app.get('/stream', (req, res)=>{
+    res.set('content-type', 'text/event-stream');
+
+    setInterval(()=>{
+        let id = (new Date()).getTime();
+        res.write(`id: ${id}\n`);
+        res.write('data: My message\ndata: My second Message\n\n');
+    }, 5000);
+});
+
+app.get('/stream/json', (req, res) => {
+    let id = (new Date()).getTime();
+    res.set('content-type', 'text/event-stream');
+    let x = {
+        'a': 1,
+        'b': 2
+    };
+    res.write(`id: ${id}\n`);
+    res.write(sse_json_cons(x));
+    res.end();
 });
 
 app.listen(port, ()=>{
     console.log(`Server started on ${port}`);
 });
+
+function sse_json_cons(json){
+    return `data: ${JSON.stringify(json)}\n\n`;
+}
--- public/app.js
+++ public/app.js
@@ -1,3 +1,48 @@
 (function(){
-    console.log('Hello');
+    if (!!window.EventSource) {
+        sse_text();
+        sse_json();
+    } else {
+        console.log("Result to xhr polling :(");
+    }
+
+    function sse_text(){
+        var source = new EventSource('/stream');
+        let count = 1;
+        source.addEventListener('message', e => {
+            console.log(e.data);
+            count = count + 1;
+            if(count === 3){
+                source.close();
+            }
+        }, false);
+        source.addEventListener('open', function(e) {
+            // Connection was opened.
+        }, false);
+
+        source.addEventListener('error', function(e) {
+            if (e.readyState == EventSource.CLOSED) {
+                console.log("Connection was closed.")
+            }
+        }, false);
+
+    }
+
+    function sse_json(){
+        var source = new EventSource('/stream/json');
+        source.addEventListener('message', e => {
+            let response = JSON.parse(e.data);
+            console.log(response.a, response.b);
+            source.close();
+        }, false);
+        source.addEventListener('open', function(e) {
+            // Connection was opened.
+        }, false);
+
+        source.addEventListener('error', function(e) {
+            if (e.readyState == EventSource.CLOSED) {
+            // Connection was closed.
+            }
+        }, false);
+    }
 })();
